38e0c38d1853dfd1e8d4d474503caf2ae0918c91
Git/Github-Practice <kamaluddinshaikh14@gmail.com>
2024-01-30 18:35:04 +0530

    C code

---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <C:\Users\KamaluddinShaikh\Downloads\Git Demo\LocalRepo\hello.c>
+int main() {
+    // Print "Hello World" to the console
+    printf("Hello World\n");
+
+    // Return 0 to indicate successful execution
+    return 0;
+}
